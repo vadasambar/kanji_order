b14c62ead135b6196ea76fcb612ecaf3b442dab9
Retrazil <retrazil@gmail.com>
2017-06-26 18:32:33 +0530

    fix disappearing output

--- search.js
+++ search.js
@@ -1,17 +1,14 @@
 
 function getPageNumber(){
 
-var kanji = document.getElementById("search-box").value;
-alert(typeof(kanji));
-if(kanji == null){
- document.getElementById("results").innerHTML = "NULL";  
-}
+    var kanji = document.getElementById("search-box").value;
+    if(kanji == null){
+        document.getElementById("results").innerHTML = "NULL";  
+    }
 
-document.getElementById("results").innerHTML = "IT IS WORKING"; 
-for(var prop in database){
-  if(prop == kanji){
-    alert("KANJI FOUND");
-    document.getElementById("results").innerHTML = prop + " : " + database[prop]["page"] + "<br>"; 
-  }
-}
+    for(var prop in database){
+        if(prop == kanji){
+            document.getElementById("results").innerHTML = prop + " : " + database[prop]["page"] + "<br>"; 
+        }
+    }
 }
