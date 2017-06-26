e3f5344971a251809c27a5abbbd55d7dd23737db
Retrazil <retrazil@gmail.com>
2017-06-26 21:34:18 +0530

    reformat results, add id and meaning

--- search.js
+++ search.js
@@ -5,10 +5,14 @@ function getPageNumber(){
     if(kanji == null){
         document.getElementById("results").innerHTML = "NULL";  
     }
-
+    
+    
+    document.getElementById("results").innerHTML = kanji + "<br>";
     for(var prop in database){
         if(prop == kanji){
-            document.getElementById("results").innerHTML = prop + " : " + database[prop]["page"] + "<br>"; 
+            document.getElementById("results").innerHTML += "page" + " : " + database[prop]["page"] + "<br>"
+                                                          + "id" + " : " + database[prop]["id"] + "<br>"
+            + "meaning" + " : " + database[prop]["meaning"] + "<br>";
         }
     }
 }
