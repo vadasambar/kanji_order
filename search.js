dbe82e5028c308cfb310eaa15ffb64baa150c5cd
Retrazil <retrazil@gmail.com>
2017-06-27 16:37:26 +0530

    add multiple character support & enter key submit

--- search.js
+++ search.js
@@ -1,18 +1,22 @@
 
 function getPageNumber(){
 
-    var kanji = document.getElementById("search-box").value;
-    if(kanji == null){
-        document.getElementById("results").innerHTML = "NULL";  
-    }
-    
-    
-    document.getElementById("results").innerHTML = kanji + "<br>";
+  var kanjiList = document.getElementById("search-box").value;
+  if(kanjiList == null){
+    document.getElementById("results").innerHTML = "got NULL :/ <br> Enter a kanji baka yaro >_<";  
+  }  
+
+document.getElementById("results").innerHTML = "";
+  
+  for (var i = 0; i < kanjiList.length; i++ ){
+    var kanji = kanjiList [i];
+    document.getElementById("results").innerHTML += kanji + "<br>";
     for(var prop in database){
-        if(prop == kanji){
-            document.getElementById("results").innerHTML += "page" + " : " + database[prop]["page"] + "<br>"
-                                                          + "id" + " : " + database[prop]["id"] + "<br>"
-            + "meaning" + " : " + database[prop]["meaning"] + "<br>";
-        }
+      if(prop == kanji){
+        document.getElementById("results").innerHTML += "page" + " : " + database[prop]["page"] + "<br>"
+          + "id" + " : " + database[prop]["id"] + "<br>"
+          + "meaning" + " : " + database[prop]["meaning"] + "<br><br>";
+      }
     }
+  }
 }
