ab00823a4b218c7a05a62bf2d2df3650c791071a
Retrazil <retrazil@gmail.com>
2017-06-28 18:30:28 +0530

    add search by kanji id

--- search.js
+++ search.js
@@ -1,22 +1,45 @@
 
 function getPageNumber(){
 
-  var kanjiList = document.getElementById("search-box").value;
-  if(kanjiList == null){
-    document.getElementById("results").innerHTML = "got NULL :/ <br> Enter a kanji baka yaro >_<";  
-  }  
+    var kanjiList = document.getElementById("search-box").value; // get user input
+    if(kanjiList == null){ 
+        document.getElementById("results").innerHTML = "got NULL :/ <br> Enter a kanji baka yaro >_<";  
+    }  
 
-document.getElementById("results").innerHTML = "";
+    document.getElementById("results").innerHTML = ""; // clean all text of previous search
+    
+    if(!isNaN (parseInt(kanjiList))){
+        searchByID(kanjiList); 
   
-  for (var i = 0; i < kanjiList.length; i++ ){
-    var kanji = kanjiList [i];
-    document.getElementById("results").innerHTML += kanji + "<br>";
-    for(var prop in database){
-      if(prop == kanji){
-        document.getElementById("results").innerHTML += "page" + " : " + database[prop]["page"] + "<br>"
-          + "id" + " : " + database[prop]["id"] + "<br>"
-          + "meaning" + " : " + database[prop]["meaning"] + "<br><br>";
-      }
+    } else {
+        searchByKanji(kanjiList); 
+    }   
+
+}
+
+function printResults (kanji){
+    document.getElementById("results").innerHTML += kanji + "<br>" 
+                                                  + "page" + " : " + database[kanji]["page"] + "<br>"
+                                                  + "id" + " : " + database[kanji]["id"] + "<br>"
+                                                  + "meaning" + " : " + database[kanji]["meaning"] + "<br><br>";  
+}
+
+function searchByID(kanjiList){
+    var id = kanjiList; 
+    for(var kanji in database){
+        if(id == database[kanji]["id"]){
+            printResults(kanji); 
+        }
     }
-  }
+}
+
+function searchByKanji (kanjiList){
+    for (var i = 0; i < kanjiList.length; i++){
+        var kanji = kanjiList [i]; 
+        for (var prop in database){
+            if (prop == kanji){
+                printResults (kanji); 
+            }
+        }
+    }
 }
