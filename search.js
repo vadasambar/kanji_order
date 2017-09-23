0912863f82213f07f2681b97cdf467f7394f9f48
Retrazil <retrazil@gmail.com>
2017-09-23 17:27:50 +0530

    major: change kanji block layout
    changes:
    1. wrap kanji display parts in css blocks
    2. create a block like display
    3. refactor kanji display code in js

--- search.js
+++ search.js
@@ -1,68 +1,72 @@
 
 function getPageNumber(){
 
-    var kanjiList = document.getElementById("search-box").value; // get user input
-    if(kanjiList == null){ 
-        document.getElementById("results").innerHTML = "got NULL :/ <br> Enter a kanji baka yaro >_<";  
-    }  
+  var kanjiList = document.getElementById("search-box").value; // get user input
+  if(kanjiList == null){ 
+    document.getElementById("results").innerHTML = "got NULL :/ <br> Enter a kanji baka yaro >_<";  
+  }  
 
-    document.getElementById("results").innerHTML = ""; // clean all text of previous search
-    
-    if(!isNaN (parseInt(kanjiList))){
-        searchByID(kanjiList);  // if input is number id
-    }
+  document.getElementById("results").innerHTML = ""; // clean all text of previous search
+  
+  if(!isNaN (parseInt(kanjiList))){
+    searchByID(kanjiList);  // if input is number id
+  }
 
-    if(isLetter(kanjiList[0])){
-        searchByMeaning(kanjiList); // if input is character
-    }  
-    else {
-        searchByKanji(kanjiList); // if input is kanji
-    }   
+  if(isLetter(kanjiList[0])){
+    searchByMeaning(kanjiList); // if input is character
+  }  
+  else {
+    searchByKanji(kanjiList); // if input is kanji
+  }   
 
 }
 
 function isLetter(c) {
-    return c.toLowerCase() != c.toUpperCase();
+  return c.toLowerCase() != c.toUpperCase();
 }
 
 function printResults (kanji){
-    document.getElementById("results").innerHTML += kanji + "<br>" 
-                                                  + "page" + " : " + database[kanji]["page"] + "<br>"
-                                                  + "id" + " : " + database[kanji]["id"] + "<br>"
-                                                  + "meaning" + " : " + database[kanji]["meaning"] + "<br><br>";  
+  kanjiBlock = "<div id='kanji-block'><h2>" +  database[kanji]["page"] + "</h2>"                                                 
+    + "<div id='character'>" + kanji + "</div>" 
+    // + "page" + " : " + database[kanji]["page"] + "<br>"
+    + "<div><span id='id'>" + database[kanji]["id"] + "</span></div>"
+    + "<div id='meaning'>" + database[kanji]["meaning"] + "</div></div>";
+
+//  document.getElementById("character").innerHTML += kanji; 
+  document.getElementById("results").innerHTML += kanjiBlock; // append to 'results' for multiple kanjis 
 }
 
 function searchByID(kanjiList){
-    var id = kanjiList; 
-    for(var kanji in database){
-        if(id == database[kanji]["id"]){
-            printResults(kanji); 
-        }
+  var id = kanjiList; 
+  for(var kanji in database){
+    if(id == database[kanji]["id"]){
+      printResults(kanji); 
     }
+  }
 }
 
 function searchByKanji (kanjiList){
-    for (var i = 0; i < kanjiList.length; i++){
-        var kanji = kanjiList [i]; 
-        for (var prop in database){
-            if (prop == kanji){
-                printResults (kanji); 
-            }
-        }
+  for (var i = 0; i < kanjiList.length; i++){
+    var kanji = kanjiList [i]; 
+    for (var prop in database){
+      if (prop == kanji){
+        printResults (kanji); 
+      }
     }
+  }
 }
 
 function searchByMeaning(kanjiList){
-    var name = kanjiList; 
-    for(var kanji in database){
-        var meaningList = database[kanji]["meaning"].split(/,/); // store multiple meanings in an array
-        for(var index in meaningList){
-            meaning = meaningList[index].trim(); // remove white spaces before and after meaning
-            if(name.toUpperCase() == meaning){
-                printResults(kanji);
-            }
-        }
+  var name = kanjiList; 
+  for(var kanji in database){
+    var meaningList = database[kanji]["meaning"].split(/,/); // store multiple meanings in an array
+    for(var index in meaningList){
+      meaning = meaningList[index].trim(); // remove white spaces before and after meaning
+      if(name.toUpperCase() == meaning){
+        printResults(kanji);
+      }
     }
+  }
 }
 
 
